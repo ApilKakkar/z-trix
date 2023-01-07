929b904375975f2c85aa87f9ee273d46e80c5fd2
Apil Kakkar <apilkakkar@Apils-MacBook-Air.local>
2023-01-07 22:02:29 +0530

    camera movement with scroll and cursor added

--- src/Experience/Camera.ts
+++ src/Experience/Camera.ts
@@ -11,6 +11,12 @@ export default class Camera
     canvas: HTMLCanvasElement
     instance: any
     controls: any
+    scrollY: number
+    cursor: { x: number; y: number }
+    parallaxY: number
+    parallaxX: number
+    instanceGroup: any
+    time: import("/Users/apilkakkar/Personal/z-trix/src/Experience/Utils/Time").default
     constructor()
     {   
         this.experience = new Experience(HTMLCanvasElement)
@@ -18,11 +24,34 @@ export default class Camera
         this.sizes = this.experience.sizes
         this.scene = this.experience.scene
         this.canvas = this.experience.canvas
+        this.time = this.experience.time
+
+        // for camera movement with scroll
+
+        this.scrollY = window.scrollY
+
+        window.addEventListener('scroll',()=>{
+            this.scrollY = window.scrollY
+        })
+
+        // for camera movement with cursor
+
+        this.cursor = {x:0,y:0}
+
+        window.addEventListener('mousemove',(event)=>{
+            this.cursor.x = event.clientX / this.sizes.width - 0.5
+            this.cursor.y = event.clientY / this.sizes.height - 0.5
+        })
 
         this.setInstance()
         // this.setOrbitControls()
     }
     setInstance() {
+        // group for camera instance
+        this.instanceGroup = new THREE.Group()
+        this.scene.add(this.instanceGroup)
+
+        // camera insatance
         this.instance = new THREE.PerspectiveCamera(
             35,
             this.sizes.width / this.sizes.height,
@@ -31,7 +60,9 @@ export default class Camera
         )
 
         this.instance.position.set(0, 0, 6)
-        this.scene.add(this.instance)
+
+        this.instanceGroup.add(this.instance)
+        
     }
 
     setOrbitControls() {
@@ -48,5 +79,14 @@ export default class Camera
     update() {
         // for orbit controls
         // this.controls.update()
+
+        this.instance.position.y = - (this.scrollY / this.sizes.height) * this.experience.geometries.objectsDistance
+
+        this.parallaxX = this.cursor.x
+        this.parallaxY = this.cursor.y
+        
+        // we are doing easing by using lerping formula and also using deltatime so that it looks same on every pc.
+        this.instanceGroup.position.x = - (this.parallaxX - this.instanceGroup.position.x) * 5 * (this.time.delta / 1000)
+        this.instanceGroup.position.y = (this.parallaxY - this.instanceGroup.position.y) * 5 * (this.time.delta / 1000)
     }
 }
--- src/Experience/Geometries.ts
+++ src/Experience/Geometries.ts
@@ -7,15 +7,28 @@ export default class Geometries
     scene: any
     material: any
     datgui: any
+    textureLoader: any
+    gradientTexture: any
+    sectionMeshes: any[]
+    elapsed: number
+    objectsDistance: number
     constructor(){
 
         this.experience = new Experience(HTMLCanvasElement)
         this.scene = this.experience.scene
         this.datgui = this.experience.datgui
 
+        //textures
+        this.textureLoader = new THREE.TextureLoader()
+        this.gradientTexture = this.textureLoader.load('static/textures/gradients/3.jpg')
+        this.gradientTexture.magFilter = THREE.NearestFilter
 
         //objects
-        this.material = new THREE.MeshToonMaterial({color : this.datgui.parameters.materialColor})
+        this.objectsDistance = 4
+        this.material = new THREE.MeshToonMaterial({
+            color : this.datgui.parameters.materialColor,
+            gradientMap : this.gradientTexture
+        })
 
         const mesh1 = new THREE.Mesh(
             new THREE.TorusGeometry(1, 0.4, 16, 60),
@@ -29,10 +42,31 @@ export default class Geometries
             new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
             this.material
         )
+
+        mesh1.position.y = - this.objectsDistance * 0
+        mesh2.position.y = - this.objectsDistance * 1
+        mesh3.position.y = - this.objectsDistance * 2
+
+        mesh1.position.x = 2
+        mesh2.position.x = - 2
+        mesh3.position.x = 2
+
         this.scene.add(mesh1, mesh2, mesh3)
+
+        this.sectionMeshes = [mesh1,mesh2,mesh3]
     }
 
     datguichange() {
         this.material.color.set(this.datgui.parameters.materialColor)
+    }
+
+    update() {
+        // rotation variables
+        this.elapsed = this.experience.time.elapsed / 1000
+
+        for( const mesh of this.sectionMeshes){
+            mesh.rotation.x = this.elapsed * 0.1
+            mesh.rotation.y = this.elapsed * 0.12
+        }
     }
 }
